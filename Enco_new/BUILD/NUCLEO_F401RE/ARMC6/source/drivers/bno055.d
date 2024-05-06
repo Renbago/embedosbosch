eea0db295d4b67afd35bf7583de083bfec2c2539
KOU-Mekatronom <mekatronom.kou@gmail.com>
2024-05-06 21:59:35 +0300

    embedos

---
+++ Enco_new/BUILD/NUCLEO_F401RE/ARMC6/source/drivers/bno055.d
@@ -0,0 +1,3 @@
+BUILD\NUCLEO_F401RE\ARMC6\source\drivers\bno055.o: \
+  source\drivers\bno055.cpp BUILD\NUCLEO_F401RE\ARMC6\mbed_config.h \
+  include\drivers\bno055.hpp
---
+++ Enco_new/BUILD/NUCLEO_F401RE/ARMC6/source/utils/queue.d
@@ -0,0 +1,3 @@
+BUILD\NUCLEO_F401RE\ARMC6\source\utils\queue.o: source\utils\queue.cpp \
+  BUILD\NUCLEO_F401RE\ARMC6\mbed_config.h include\utils\queue.hpp \
+  include\utils\queue.tpp
